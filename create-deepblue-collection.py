2222b30efeedba791ad004001dc6f78854fadcb5
Max Eckard <eckardm@umich.edu>
2015-09-28 12:02:57 -0400

    updates

--- create-deepblue-collection.py
+++ create-deepblue-collection.py
@@ -1,89 +1,68 @@
-'''
-Here's what we need to do:
-  * Figure out what we need to know to create a new collection.
-  * Make functions that return the appropriate string or boolean for each of those things.
-  * Create a script that will add a new collection.'''
-  
-'''
-<p>
-<form style="display:inline" action="http://deepblue.lib.umich.edu/handle/2027.42/XXXXXX/browse?">
-<input type="submit" value="View all items in this collection"/>
-</form>
-</p>
-<br />
-
-
-<p><img src="http://deepblue.lib.umich.edu/static/image/bentley-banner.jpg" alt="Bentley Historical Library banner" /></p>
-
-<h2> XXX </h2>
-
-<p>The materials in this online repository form part of a larger XXXX record group / manuscript collection held by the <a href="http://bentley.umich.edu/">Bentley Historical Library</a>.  For a more complete index to the materials, please consult the collection's <a href="http://quod.lib.umich.edu/b/bhlead/umich-bhl-XXXXX">online finding aid</a>.
-</p>
-
-<p>The materials in this online repository form part of a larger XXXX record group / manuscript collection held by the <a href="http://bentley.umich.edu/">Bentley Historical Library</a>.  For a more complete index to the materials, please consult the online finding aids for the:
-<ul>
-  <li><a href="http://quod.lib.umich.edu/b/bhlead/umich-bhl-XXXXX">XXXXX</a></li>
-  <li><a href="http://quod.lib.umich.edu/b/bhlead/umich-bhl-XXXXX">XXXXX</a></li>
-</ul>
-</p>
-
-<p>
-Researchers may also be interested in the <a href="http://webarchives.cdlib.org/site/XXXXXX">XXXXX Web Archives</a>.
-</p>
-
-<p>
-For questions or more information, please contact the Bentley Historical Library's <a href="mailto:bentley.ref@umich.edu">Division of Reference and Access Services</a>
-</p>
-
-<p>
-<b>Abstract:</b><br />
-
-</p>
-
-<p>
-<b>History / Biography:</b><br />
-
-</p>
-
-COPYRIGHT
-<h2>Please note:</h2>
-<p>Copyright has been transferred to the Regents of the University of Michigan.</p>
-<br />
-<br />
-
-<p>
-Access to digitized sound recordings may be limited to the reading room of the <a href="http://bentley.umich.edu/">Bentley Historical Library</a>, located on the Ann Arbor campus of the University of Michigan.</p> 
-
-LICENSE
-As the designated coordinator for this Deep Blue Collection, I am authorized by the Community members to serve as their representative in all dealings with the Repository. As the designee, I ensure that I have read the Deep Blue policies. Furthermore, I have conveyed to the community the terms and conditions outlined in those policies, including the language of the standard deposit license quoted below and that the community members have granted me the authority to deposit content on their behalf.'''
+from 
 
 # selenium is used automate web browser interaction, you'll need to install it
 import selenium
 
-# beautifulsoup pulls data out of html files, you'll need to install it
-from bs4 import BeautifulSoup
-
 # name
 name_text = raw_input('Name: ')
+print name_text
 
 # introductory text
-view_all_items_in_this_collection = 
+# view all items in this collection
+view_all_items_in_this_collection_text = open('view-all-items-in-this-collection.txt', 'r').read()
+
+# bentley historical library banner
+bentley_historical_library_banner_text = open('bentley-historical-library-banner.txt', 'r').read()
 
 # h2 section
+h2_text = '<h2>' + name_text + '</h2>'
 
-# is this part of a larger record group or manuscript collection?
-# if yes, which one? if yes, are there many finding aids or just one? <-- out of scope this first time
-# web archives?
-# where are the finding aids?
+# record group or manuscript collection
+record_group_or_manuscript_collection_input = raw_input('Is it part of a larger record group or manuscript collection?')
+if record_group_or_manuscript_collection_input == 'yes':
+    record_group_input = raw_input('Which one?')
+    if record_group_input == 'record group':
+        eadid_text = raw_input('EAD ID: ')
+        record_group_text = open('record-group.txt', 'r').read().replace('XXXXX', eadid_text).replace('XXXX', name_text).replace(' / manuscript collection', '')
+    elif record_group_input == 'manuscript collection':
+        eadid_text = raw_input('EAD ID: ')
+        record_group_text = open('record-group.txt', 'r').read().replace('XXXXX', eadid_text).replace('XXXX', name_text).repalce('record groupt / ', '')
+
+# division of reference and access services
+division_of_reference_and_access_services_text = open('division-of-reference-and-access-services.txt', 'r').read()
+
 # abstract
-# history or biography? then text
-# digitized sound records?
+abstract = raw_input('Abstract (HTML): ')
+abstract_text = '<p><b>Abstract:</b><br />' + abstract + '</p>'
 
-# copyright text
+# history or biography
+history_or_biography_input = raw_input('Does this collection have a history or biography?')
+history_or_biography_text_input = raw_input('What is it (HTML)?')
+if history_or_biography_input == 'history':
+    history_or_biography_text = '<p><b>History:</b><br />' + history_or_biography_text_input + '</p>'
+elif history_or_biography_input == 'biography':
+    history_or_biography_text = '<p><b>Biography:</b><br />' + history_or_biography_text_input + '</p>'
+
+introductory_text = view_all_items_in_this_collection_text + bentley_historical_library_banner_text + h2_text + record_group_text + division_of_reference_and_access_services_text + abstract_text + history_or_biography_text
+print introductory_text
+
+# copyright
+copyright_text = open('copyright.txt', 'r').read()
+print copyright_text
 
 # liscense
+liscense_text = 'As the designated coordinator for this Deep Blue Collection, I am authorized by the Community members to serve as their representative in all dealings with the Repository. As the designee, I ensure that I have read the Deep Blue policies. Furthermore, I have conveyed to the community the terms and conditions outlined in those policies, including the language of the standard deposit license quoted below and that the community members have granted me the authority to deposit content on their behalf.'
+print liscense_text
+
+# create deep blue collection
+
 
 # assign roles
 
 # go back and add handle
 
+'''
+known issues'''
+# anything where there is raw input needs to be debugged
+# more than one record group or manuscript collection
+# copyright won't always be transferred
