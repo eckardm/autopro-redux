a1457fd328505211aa4b223f2fffcc2ecfd3efbe
Max Eckard <eckardm@umich.edu>
2015-09-28 09:59:42 -0400

    what i think we'll need

--- create-deepblue-collection.py
+++ create-deepblue-collection.py
@@ -4,4 +4,9 @@ Here's what we need to do:
   * Make functions that return the appropriate string or boolean for each of those things.
   * Create a script that will add a new collection.'''
 
-  
+# selenium is used automate web browser interaction, you'll need to install it
+import selenium
+
+# beautifulsoup pulls data out of html files, you'll need to install it
+from bs4 import BeautifulSoup
+
