787b1e3e36e5daf2fda013b48c9afcd242d04570
Max Eckard <eckardm@umich.edu>
2015-09-28 09:51:25 -0400

    starting over

--- create-deepblue-collection.py
+++ create-deepblue-collection.py
@@ -3,4 +3,5 @@ Here's what we need to do:
   * Figure out what we need to know to create a new collection.
   * Make functions that return the appropriate string or boolean for each of those things.
   * Create a script that will add a new collection.'''
+
   
